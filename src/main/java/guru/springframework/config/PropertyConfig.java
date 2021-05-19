e0cf438d32647c40415dd93722eaf8ef9dc766be
Wendell Robles <wendellrobles@yahoo.com>
2021-05-18 20:59:54 -0500

    Updated

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -29,19 +29,23 @@ public class PropertyConfig {
 
   @Bean
   public FakeDataSource fakeDataSource() {
+
     FakeDataSource fakeDataSource = new FakeDataSource();
     fakeDataSource.setUser(user);
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
+
     return fakeDataSource;
   }
 
   @Bean
   public FakeJmsBroker fakeJmsBroker() {
+
     FakeJmsBroker jmsBroker = new FakeJmsBroker();
     jmsBroker.setUsername(jmsUsername);
     jmsBroker.setPassword(jmsPassoword);
     jmsBroker.setUrl(jmsUrl);
+
     return jmsBroker;
   }
 
