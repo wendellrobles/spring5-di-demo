6a8afdef9a9b6d13ce242e9b8099d541180e51e6
Wendell Robles <wendellrobles@yahoo.com>
2021-05-19 23:41:20 -0500

    Updated

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -10,16 +10,16 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DiDemoApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+  public static void main(String[] args) {
+    ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("myController");
+    MyController controller = (MyController) ctx.getBean("myController");
 
-		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+    FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-		System.out.println(fakeDataSource.getUser());
+    System.out.println(fakeDataSource.getUser());
 
-		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
-		System.out.println(fakeJmsBroker.getUsername());
-	}
+    FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+    System.out.println(fakeJmsBroker.getUsername());
+  }
 }
--- src/main/java/guru/springframework/examplebeans/FakeDataSource.java
+++ src/main/java/guru/springframework/examplebeans/FakeDataSource.java
@@ -1,34 +1,32 @@
 package guru.springframework.examplebeans;
 
-/**
- * Created by jt on 6/7/17.
- */
 public class FakeDataSource {
-    private String user;
-    private String password;
-    private String url;
 
-    public String getUser() {
-        return user;
-    }
+  private String user;
+  private String password;
+  private String url;
 
-    public void setUser(String user) {
-        this.user = user;
-    }
+  public String getUser() {
+    return user;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public void setUser(String user) {
+    this.user = user;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public String getUrl() {
-        return url;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
 }
