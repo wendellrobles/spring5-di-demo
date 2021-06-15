4540848f89343039ce626414e5956f005ea7d6a5
Wendell Robles <wendellrobles@yahoo.com>
2021-06-14 19:13:51 -0500

    Updated

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,18 +1,15 @@
 package guru.springframework.services;
 
-/**
- * Created by jt on 5/24/17.
- */
 public class PrimarySpanishGreetingService implements GreetingService {
 
-    private GreetingRepository greetingRepository;
+  private GreetingRepository greetingRepository;
 
-    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
-        this.greetingRepository = greetingRepository;
-    }
+  public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+    this.greetingRepository = greetingRepository;
+  }
 
-    @Override
-    public String sayGreeting() {
-        return greetingRepository.getSpanishGreeting();
-    }
+  @Override
+  public String sayGreeting() {
+    return greetingRepository.getSpanishGreeting();
+  }
 }
