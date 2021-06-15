c44898f224b48b0845a556f0c1a3d841959a37ad
Wendell Robles <wendellrobles@yahoo.com>
2021-06-14 19:04:33 -0500

    Updated

--- src/main/java/guru/springframework/CustomBeanPostProcessor.java
+++ src/main/java/guru/springframework/CustomBeanPostProcessor.java
@@ -4,27 +4,25 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by jt on 6/5/17.
- */
 @Component
 public class CustomBeanPostProcessor implements BeanPostProcessor {
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 
-        if(bean instanceof LifeCycleDemoBean){
-            ((LifeCycleDemoBean) bean).beforeInit();
-        }
+  @Override
+  public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 
-        return bean;
+    if (bean instanceof LifeCycleDemoBean) {
+      ((LifeCycleDemoBean) bean).beforeInit();
     }
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if(bean instanceof LifeCycleDemoBean){
-            ((LifeCycleDemoBean) bean).afterInit();
-        }
+    return bean;
+  }
 
-        return bean;
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    if (bean instanceof LifeCycleDemoBean) {
+      ((LifeCycleDemoBean) bean).afterInit();
     }
+
+    return bean;
+  }
 }
