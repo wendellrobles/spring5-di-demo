f9cc1f958297f58fe5dded8029819d0e9ed8dfeb
Wendell Robles <wendellrobles@yahoo.com>
2021-07-10 01:02:50 -0500

    Updated

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * This example shows how to set default profile in Spring.
+ */
 @SpringBootApplication
 public class DiDemoApplication {
 
